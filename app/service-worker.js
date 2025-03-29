10acccbd886de2f9627afa1f836dd3bd60498bd2
Keira <keiraJcoder@hotmail.com>
2025-03-29 14:58:02 +0000

    updates to service-worker

--- app/service-worker.js
+++ app/service-worker.js
@@ -2,14 +2,14 @@ self.addEventListener('install', event => {
     event.waitUntil(
       caches.open('spoons-cache').then(cache => {
         return cache.addAll([
-          '/',
-          '/app/index.html',
-          '/app/style.css',
-          '/app/script.js',
-          '/app/confetti.js',
-          '/app/manifest.json',
-          '/app/ico-192.png',  // Add icon-192.png here
-          '/app/ico-512.png'   // Add icon-512.png here
+          './',
+          './app/index.html',
+          './app/style.css',
+          './app/script.js',
+          './app/confetti.js',
+          './app/manifest.json',
+          './app/ico-192.png',  // Add icon-192.png here
+          './app/ico-512.png'   // Add icon-512.png here
         ]);
       })
     );
